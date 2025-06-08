434ae9357e9a2cfa0cd1d4f7fbc9c34eef7ca4b4
balagesh <balagesh.h@gmail.com>
2025-06-08 11:39:45 +0200

    SRP

--- GuesserGame.java
+++ GuesserGame.java
@@ -1,12 +1,22 @@
-//target number is random
+//towards single responsability
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuesserGame {
 	public static void main(String[] args) {
-		Random rand = new Random();
-		int target = rand.nextInt(101);
+		int target = rand();
 		System.out.println("Let's play! It's time to guess.\nA number between 0 and 100.");
+		scan(target);
+		System.out.println("Congrats!");		
+	}
+	
+	public static int rand() {
+	    Random rand = new Random();
+		int target = rand.nextInt(101);
+		return target;
+	}
+	
+	public static void scan(int target) {
 		Scanner sc = new Scanner(System.in);
 		int guess = sc.nextInt();
 		while (guess != target) {
@@ -16,7 +26,7 @@ public class GuesserGame {
 		        System.out.println("Too low!");
 		    }
 		guess = sc.nextInt();
-		}	
-		System.out.println(target);		
+		}
+		sc.close();		    
 	}
 }
