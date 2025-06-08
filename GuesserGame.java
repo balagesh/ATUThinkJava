9a694a61bbfb02e6c1e93b477c8236acf573b06e
balagesh <balagesh.h@gmail.com>
2025-06-08 18:28:14 +0200

    GuesserGame: restart

--- GuesserGame.java
+++ GuesserGame.java
@@ -1,14 +1,30 @@
-//towards single responsability
+//restart
 import java.util.Random;
 import java.util.Scanner;
 
 public class GuesserGame {
 	public static void main(String[] args) {
+	       Scanner sc = new Scanner(System.in);
+	       letsPlay(sc);
+	       System.out.println("Thanks for playing with me!");
+	       sc.close();
+	}
+	
+	public static void letsPlay(Scanner sc) {
 		int target = rand();
 		System.out.println("Let's play! It's time to guess.\nA number between 0 and 100.");
-		scan(target);
-		System.out.println("Congrats!");		
+		scan(sc, target);
+		System.out.println("Congrats!");
+		nextRound(sc);			    
 	}
+	
+	public static void nextRound(Scanner sc) {
+	       System.out.println("One more? (yes/no)");
+	       String answer = sc.nextLine();
+	       if (answer.equalsIgnoreCase("yes")) {
+	           letsPlay(sc);
+        }
+    }
 	
 	public static int rand() {
 	    Random rand = new Random();
@@ -16,17 +32,16 @@ public class GuesserGame {
 		return target;
 	}
 	
-	public static void scan(int target) {
-		Scanner sc = new Scanner(System.in);
-		int guess = sc.nextInt();
+	public static void scan(Scanner sc, int target) {
+		int guess = -77;
+
 		while (guess != target) {
+		    guess = Integer.parseInt(sc.nextLine());
 		    if (guess > target) {
 		    System.out.println("Too high!");}
 		    else if (guess != target) {
 		        System.out.println("Too low!");
 		    }
-		guess = sc.nextInt();
-		}
-		sc.close();		    
+		}  
 	}
 }
