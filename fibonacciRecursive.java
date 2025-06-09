a04d0b679aee11c47b1423cb99d86cd6939394ee
balagesh <balagesh.h@gmail.com>
2025-06-09 19:51:30 +0200

    Fibonacci with recursion - no for loop

---
+++ fibonacciRecursive.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int limit = 8;
+        scan(0, limit);
+    }
+
+    public static void scan(int index, int limit) {
+        if (index == limit) {
+            return;
+        }
+        System.out.print(fib(index) + " ");
+        scan(index + 1, limit);
+    }
+
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}
